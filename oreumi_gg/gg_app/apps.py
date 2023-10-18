7bbb08dceb0c8b0a675f04e5175666c3be013956
animalwook <youngwook616@gmail.com>
2023-10-18 10:27:16 +0900

    init

---
+++ oreumi_gg/gg_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GgAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gg_app'
---
+++ oreumi_gg/gg_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+ 
+app_name = 'gg_app'    #score 어플리케이션의 url 호출시 앞의 구분자 사용을 하시려면 작성합시다.
+ 
+urlpatterns = [
+    path('', views.index, name='index'),       
+]
