77e413af42022ba0ea48dbe61be762d782bafb95
animalwook <youngwook616@gmail.com>
2023-10-18 11:25:26 +0900

    블로그 리스트

--- oreumi_gg/gg_app/models.py
+++ oreumi_gg/gg_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200,) 
+    content = models.TextField() # 에디터로 바뀔예정
+
+    author = models.CharField(max_length=200,default="admin") # 나중에 외래키로
+    view = models.IntegerField(default=0)
+    category = models.CharField(max_length=64, default="기본") 
+    # images = models.ImageField(null=True,upload)
+    # video =
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    up = models.IntegerField(default=0)
+    down = models.IntegerField(default=0)
+
--- oreumi_gg/gg_app/views.py
+++ oreumi_gg/gg_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from .models import BlogPost
 # Create your views here.
 
 
 def index(request):
-    return render(request,"index.html")
+    blog_post = BlogPost.objects.all()
+    return render(request,"index.html",{"posts":blog_post})
