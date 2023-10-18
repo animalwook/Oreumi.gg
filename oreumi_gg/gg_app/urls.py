5d42b3681d69575eef75656373118b375a0c2b98
Hwang-99 <Silr0n0618@naver.com>
2023-10-18 17:35:06 +0900

    [Add] urls.py 및 views.py 추가 작성 (테스트용)

--- oreumi_gg/gg_app/urls.py
+++ oreumi_gg/gg_app/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'gg_app'    #score 어플리케이션의 url 호출시 앞의 구분자 사용을 하시려면 작성합시다.
  
 urlpatterns = [
-    path('', views.index, name='index'),       
+    path('', views.index, name='index'),
+    path('community/', views.community, name='community'),
+    path('champions/', views.community, name='champions'),
+    path('login/', views.community, name='login'),
+    path('register/', views.community, name='register'),
 ]
