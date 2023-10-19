226d926b6f4413564f0dc88000cf919b4176b48c
UNeedStudy <zlsoq@naver.com>
2023-10-19 11:00:38 +0900

    fix. 플레이어 정보 일부 추가

--- oreumi_gg/gg_app/views.py
+++ oreumi_gg/gg_app/views.py
@@ -19,6 +19,10 @@ def search(request, country, summonername):
     return render(request)
 
 def match(country, puuid, summonername):
+    summonerSpells = {21 : "SummonerBarrier", 1 : "SummonerBoost", 14 : "SummonerDot", 3 : "SummonerExhaust", 4 : "SummonerFlash",
+                  6 : "SummonerHaste", 7 : "SummonerHeal", 13 : "SummonerMana", 30 : "SummonerPoroRecall",
+                  31 : "SummonerPoroThrow", 11 : "SummonerSmite", 39 : "SummonerSnowURFSnowball_Mark", 32: "SummonerSnowball",
+                  12 : "SummonerTeleport"}
     # 이미지 가져오는 주소
     # https://ddragon.leagueoflegends.com/cdn/13.20.1/img/champion/Yone.png
     match_20 = watcher.match.matchlist_by_puuid(country, puuid)
@@ -57,10 +61,22 @@ def match(country, puuid, summonername):
                 search_player_kill = player_info["kills"]
                 search_player_assist = player_info["assists"]
                 search_player_death = player_info["deaths"]
-                
-            player_dict = [{"kills" : player_info["kills"], "assists" : player_info["assists"], "deaths" : player_info["deaths"],
-                           "summonername" : player_info["summonerName"], "championname" : player_info["championName"], 
-                           "teamposition" : player_info["teamPosition"], "teamid" : player_info["teamId"]}]
+            totalminions_kill = player_info["totalMinionsKilled"] + player_info["neutralMinionsKilled"]
+            for key, value in summonerSpells.items():
+                if key == player_info["summoner1Id"]:
+                    summonerspell1 = value
+                if key == player_info["summoner2Id"]:
+                    summonerspell2 = value
+            player_dict = [{"kills" : player_info["kills"], "assists" : player_info["assists"], 
+                            "deaths" : player_info["deaths"], "summonername" : player_info["summonerName"], 
+                            "championname" : player_info["championName"], "teamposition" : player_info["teamPosition"], 
+                            "teamid" : player_info["teamId"], "item0" : player_info["item0"],
+                           "item1" : player_info["item1"], "item2" : player_info["item2"], 
+                           "item3" : player_info["item3"], "item4" : player_info["item4"],
+                           "item5" : player_info["item5"], "item6" : player_info["item6"],
+                           "totalminions_kill" : totalminions_kill, "wardsKilled" : player_info["wardsKilled"],
+                           "wardsPlaced" : player_info["wardsPlaced"], "visionWardsBoughtInGame" : player_info["visionWardsBoughtInGame"],
+                           "summonerspell1" : summonerspell1, "summonerspell2" : summonerspell2}]
             result[num] = player_dict
             num += 1
         result.update({"time" : time, "game_type" : game_type, "win_or_not" : win_or_not, "search_player_kill" : search_player_kill, "search_player_death" : search_player_death,
